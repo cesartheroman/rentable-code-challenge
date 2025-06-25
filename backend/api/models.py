e738ef2870fdc6ee15d0d2ceceb65b05cb3ba75f
cesartheroman <cesardroman18@gmail.com>
2025-06-25 15:03:31 -0500

    adds model file edit for prev commit

--- backend/api/models.py
+++ backend/api/models.py
@@ -21,7 +21,7 @@ class Transaction(models.Model):
     date = models.DateField()
     description = models.CharField(max_length=255)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
-    type = models.CharField(max_length=7, choices=TRANSATION_TYPES, null=True, blank=True)
+    type = models.CharField(max_length=7, choices=TRANSATION_TYPES)
 
     def __str__(self):
         return f"{self.date} - {self.description} ({self.amount})"
